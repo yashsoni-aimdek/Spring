03e5fbe7c8b0beb7f040433bc4e8d45fa3ccd88d
yashsoni-aimdek <yash.soni@aimdek.com>
2022-04-25 12:25:37 +0530

    Completed Crud Operation in Spring

---
+++ spring project/demo/src/main/java/com/example/demo/JDBCTemplate.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.dao.StudentDAO;
+
+@SpringBootApplication
+public class JDBCTemplate implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDBCTemplate.class, args);
+	}
+
+	@Autowired
+	private StudentDAO studentDAO;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
